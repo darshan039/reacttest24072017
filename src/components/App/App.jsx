9d7b5ff58b480fac61bda076537ed08d923e8280
Darshan.Joshi <darshan.joshi@pmcretail.com>
2017-07-24 13:08:56 +0530

    Added Bootstrap Styles.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <ul>
-                <li><Link to="productList">Product List</Link></li>
-                <li><Link to="shoppingCart">Shopping Cart</Link></li>
-            </ul>
-            {this.props.children}
-         </div>
-      );
-   }
+    render() {
+        var floatRight = {'float': 'right'};
+        return (
+            <div className="container-fluid">
+                <nav className="navbar navbar-inverse">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <Link className="navbar-brand" to="productList">Shopping Cart Example</Link>
+                        </div>
+                        <ul className="nav navbar-nav">
+                            <li><Link to="productList">Product List</Link></li>
+                            <li><Link to="shoppingCart">Shopping Cart</Link></li>
+                        </ul>
+                    </div>
+                </nav>
+                {this.props.children}
+            </div>
+        );
+    }
 }
